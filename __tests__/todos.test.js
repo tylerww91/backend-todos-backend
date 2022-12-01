a69f3a9c8d62988bb09417bf4876d84b72249c45
Tyler Watson <tylerww91@gmail.com>
2022-12-01 10:21:18 -0800

    builds todo's table, edits users table to not require first/last name, and builds todos test suite

---
+++ __tests__/todos.test.js
@@ -0,0 +1,36 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+    firstName: 'Test',
+    lastName: 'User',
+    email: 'test@example.com',
+    password: '12345',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+    const password = userProps.password ?? mockUser.password;
+
+    const agent = request.agent(app);
+
+    const user = await UserService.create({ ...mockUser, ...userProps });
+
+    const { email } = user;
+    await agent.post('/api/v1/users/sessions').send({ email, password })
+    return [agent, user];
+
+};
+
+describe('todos routes', () => {
+    beforeEach(() => {
+        return setup(pool);
+    });
+
+    afterAll(() => {
+        pool.end();
+    });
+
+})
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,11 +1,21 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS todos;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email VARCHAR,
   password_hash VARCHAR NOT NULL,
-  first_name VARCHAR NOT NULL,
-  last_name VARCHAR NOT NULL
+  first_name VARCHAR,
+  last_name VARCHAR
 );
+
+CREATE TABLE todos (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT,
+  description VARCHAR NOT NULL,
+  completed BOOLEAN NOT NULL DEFAULT(false),
+  created_at TIMESTAMPZ NOT NULL DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+)
