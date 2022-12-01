845de87db5a94ab3ef91830f4c544ff0eb87060f
Tyler Watson <tylerww91@gmail.com>
2022-12-01 11:36:07 -0800

    removes first/last name from users tests/sql, sets SECURE_COOKIES = false

--- __tests__/todos.test.js
+++ __tests__/todos.test.js
@@ -1,36 +1,32 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-const request = require('supertest');
-const app = require('../lib/app');
-const UserService = require('../lib/services/UserService');
+// const request = require('supertest');
+// const app = require('../lib/app');
+// const UserService = require('../lib/services/UserService');
 
-const mockUser = {
-    firstName: 'Test',
-    lastName: 'User',
-    email: 'test@example.com',
-    password: '12345',
-};
+// const mockUser = {
+//   email: 'test@example.com',
+//   password: '12345',
+// };
 
-const registerAndLogin = async (userProps = {}) => {
-    const password = userProps.password ?? mockUser.password;
+// const registerAndLogin = async (userProps = {}) => {
+//   const password = userProps.password ?? mockUser.password;
 
-    const agent = request.agent(app);
+//   const agent = request.agent(app);
 
-    const user = await UserService.create({ ...mockUser, ...userProps });
+//   const user = await UserService.create({ ...mockUser, ...userProps });
 
-    const { email } = user;
-    await agent.post('/api/v1/users/sessions').send({ email, password })
-    return [agent, user];
-
-};
+//   const { email } = user;
+//   await agent.post('/api/v1/users/sessions').send({ email, password });
+//   return [agent, user];
+// };
 
 describe('todos routes', () => {
-    beforeEach(() => {
-        return setup(pool);
-    });
+  beforeEach(() => {
+    return setup(pool);
+  });
 
-    afterAll(() => {
-        pool.end();
-    });
-
-})
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,14 +1,12 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS todos;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS todos CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email VARCHAR,
-  password_hash VARCHAR NOT NULL,
-  first_name VARCHAR,
-  last_name VARCHAR
+  password_hash VARCHAR NOT NULL
 );
 
 CREATE TABLE todos (
@@ -16,6 +14,6 @@ CREATE TABLE todos (
   user_id BIGINT,
   description VARCHAR NOT NULL,
   completed BOOLEAN NOT NULL DEFAULT(false),
-  created_at TIMESTAMPZ NOT NULL DEFAULT NOW(),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   FOREIGN KEY (user_id) REFERENCES users(id)
 )
