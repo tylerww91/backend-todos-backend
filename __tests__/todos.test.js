c2d60fdd03686d019033ed1597b88452158424a3
Tyler Watson <tylerww91@gmail.com>
2022-12-02 22:39:22 -0800

    builds POST /api/v1/todos route

--- __tests__/todos.test.js
+++ __tests__/todos.test.js
@@ -1,25 +1,25 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
-// const UserService = require('../lib/services/UserService');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
-// const mockUser = {
-//   email: 'test@example.com',
-//   password: '12345',
-// };
+const mockUser = {
+  email: 'test@example.com',
+  password: '12345',
+};
 
-// const registerAndLogin = async (userProps = {}) => {
-//   const password = userProps.password ?? mockUser.password;
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
 
-//   const agent = request.agent(app);
+  const agent = request.agent(app);
 
-//   const user = await UserService.create({ ...mockUser, ...userProps });
+  const user = await UserService.create({ ...mockUser, ...userProps });
 
-//   const { email } = user;
-//   await agent.post('/api/v1/users/sessions').send({ email, password });
-//   return [agent, user];
-// };
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
 //hope this works
 describe('todos routes', () => {
   beforeEach(() => {
@@ -28,5 +28,19 @@ describe('todos routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('POST /api/v1/todos should create a new todo as the current user', async () => {
+    const [agent, user] = await registerAndLogin();
+    const newTodo = { description: 'eat cheese' };
+    const resp = await agent.post('/api/v1/todos').send(newTodo);
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      description: newTodo.description,
+      user_id: user.id,
+      completed: false,
+      created_at: expect.any(String),
+    });
   });
 });
